0fda6163ca69e5d97503376ff541e5f6849d2f73
Viktor <viktor.sbruev.1999@gmail.com>
2025-03-10 18:03:06 +0200

    Added CW_8_3

---
+++ level_8/CW_8_3.c
@@ -0,0 +1,26 @@
+/*
+You are given an array of numbers. Return the sum of all positive numbers in the array.
+
+Вам дан массив чисел. Верните сумму всех положительных чисел в массиве.
+*/
+
+#include <stdio.h>
+
+int sum_of_positives(int arr[], int size) {
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+        if (arr[i] > 0) {
+            sum += arr[i];
+        }
+    }
+    return sum;
+}
+
+int main() {
+    int arr[] = {1, -4, 7, 12};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    
+    printf("Sum of positive numbers: %d\n", sum_of_positives(arr, size));
+    
+    return 0;
+}
