0cff2bb4b409d84200b7900d83903c9979950b37
Viktor <viktor.sbruev.1999@gmail.com>
2025-03-16 02:02:59 +0200

    Added CW_7_10

---
+++ level_7/CW_7_10.c
@@ -0,0 +1,38 @@
+/*
+Write a function that checks if a string has the same number of 'x' and 'o'.
++ Case insensitive.
++ The string may contain any characters.
++ If no 'x' or 'o' are present, return true.
+
+Напишите функцию, которая проверяет, содержит ли строка одинаковое количество символов 'x' и 'o'.
+
++ Регистр не учитывается.
++ Строка может содержать любые символы.
++ Если в строке нет 'x' и 'o', вернуть true.
+*/
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <string.h>
+
+bool XO(const char *given_string) {
+    int count_x = 0, count_o = 0;
+    
+    while (*given_string) {
+        char character = tolower(*given_string);
+        if (character == 'x') count_x++;
+        if (character == 'o') count_o++;
+        given_string++;
+    }
+    
+    return count_x == count_o;
+}
+
+int main() {
+    printf("%d\n", XO("ooxx"));
+    printf("%d\n", XO("xooxx"));
+    printf("%d\n", XO("ooxXm"));
+    printf("%d\n", XO("zpzpzpp"));
+    return 0;
+}
