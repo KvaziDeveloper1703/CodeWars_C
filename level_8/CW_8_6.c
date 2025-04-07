9ddbf3329ebf82fe182e2df8dabf700036179237
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-08 02:23:21 +0300

    Modified CW_8_6

--- level_8/CW_8_6.c
+++ level_8/CW_8_6.c
@@ -1,17 +1,19 @@
 /*
-Complete the function that takes a boolean value and returns:
-+ "Yes" if the value is True.
-+ "No" if the value is False.
+You need to create a function that takes a boolean value and returns:
++ "Yes" if the value is true;
++ "No" if the value is false.
 
-Дополните функцию, которая принимает логическое значение и возвращает:
-+ "Yes", если значение True.
-+ "No", если значение False.
+Вас нужно создать функцию, которая принимает логическое значение и возвращает:
++ "Yes", если значение true;
++ "No", если значение false.
+
+https://www.codewars.com/kata/53369039d7ab3ac506000467
 */
 
 #include <stdio.h>
 
-const char *bool_to_word(int boolean_value) {
-    if (boolean_value) {
+const char *bool_to_word(int given_boolean_value) {
+    if (given_boolean_value) {
         return "Yes";
     } else {
         return "No";
@@ -28,4 +30,3 @@ int main() {
     
     return 0;
 }
-
