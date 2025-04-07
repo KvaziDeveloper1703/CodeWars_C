267a9277c153f7fffb7e6cfd54edfcca75f71bc6
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-08 01:59:07 +0300

    Modified CW_8_4

--- level_8/CW_8_4.c
+++ level_8/CW_8_4.c
@@ -1,28 +1,30 @@
 /*
-Complete the function so that it reverses the string passed into it.
+You need to create a function that reverses the string passed into it.
 
-Дополните функцию так, чтобы она переворачивала переданную строку.
+Вам нужно создать функцию которая переворачивала бы переданную строку.
+
+https://www.codewars.com/kata/5168bb5dfe9a00b126000018
 */
 
 #include <stdio.h>
 #include <string.h>
 
-void reverse_string(char *str) {
-    int length = strlen(str);
+void reverse_string(char *given_string) {
+    int length = strlen(given_string);
     for (int i = 0; i < length / 2; i++) {
-        char temporary = str[i];
-        str[i] = str[length - i - 1];
-        str[length - i - 1] = temporary;
+        char temporary = given_string[i];
+        given_string[i] = given_string[length - i - 1];
+        given_string[length - i - 1] = temporary;
     }
 }
 
 int main() {
-    char str[100];
+    char my_string[100];
     printf("Enter a string: ");
-    scanf("%99s", str);
+    scanf("%99s", my_string);
     
-    reverse_string(str);
-    printf("Reversed string: %s\n", str);
+    reverse_string(my_string);
+    printf("Reversed string: %s\n", my_string);
     
     return 0;
 }
