c30e802a2be0630bce2d85f8164a8aec4244efb5
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-08 17:38:22 +0300

    Modified CW_7_4

--- level_7/CW_7_4.c
+++ level_7/CW_7_4.c
@@ -1,7 +1,9 @@
 /*
-In this little assignment, you are given a string of space-separated numbers and have to return the highest and lowest number.
+You are given a string of space-separated numbers and have to return the highest and lowest number.
 
-В этом небольшом задании вам дана строка, содержащая числа, разделенные пробелами. Вам нужно вернуть наибольшее и наименьшее число.
+Вам дана строка, содержащая числа, разделенные пробелами. Вам нужно вернуть наибольшее и наименьшее число.
+
+https://www.codewars.com/kata/554b4ac871d6813a03000035
 */
 
 #include <stdio.h>
@@ -10,20 +12,31 @@ In this little assignment, you are given a string of space-separated numbers and
 #include <string.h>
 
 void high_and_low(const char *given_numbers, char *result) {
+    // INT_MIN and INT_MAX are the smallest and largest possible values for an int.
+    // They help initialize min and max to extreme values for proper comparison.
     int max = INT_MIN, min = INT_MAX;
-    int number;
-    const char *ptr = given_numbers;
 
-    while (*ptr) {
-        if (sscanf(ptr, "%d", &number) == 1) {
-            if (number > max) max = number;
-            if (number < min) min = number;
-        }
-        while (*ptr && *ptr != ' ') ptr++;
-        while (*ptr == ' ') ptr++;
+    // strdup creates a duplicate of the input string because strtok modifies it.
+    // The original string is marked as const, so we work with a modifiable copy.
+    char *numbers = strdup(given_numbers);
+
+    // strtok splits the string into tokens separated by spaces.
+    // It returns a pointer to the first token, or NULL if there are none.
+    char *token = strtok(numbers, " ");
+
+    while (token) {
+        int number = atoi(token);
+        if (number > max) max = number;
+        if (number < min) min = number;
+
+        // strtok(NULL, " ") continues tokenizing from where it left off.
+        token = strtok(NULL, " ");
     }
 
     sprintf(result, "%d %d", max, min);
+
+    // Free the duplicated string to avoid memory leaks.
+    free(numbers);
 }
 
 int main() {
