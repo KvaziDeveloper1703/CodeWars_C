fef68f53c41684a4bf20ad925622c99a30e41879
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-08 23:39:49 +0300

    Modified CW_7_5

--- level_7/CW_7_5.c
+++ level_7/CW_7_5.c
@@ -1,30 +1,58 @@
 /*
-Your task is to write a function that takes any non-negative integer as an argument and returns it with its digits in descending order.
-Essentially, rearrange the digits to create the highest possible number.
+You need to write a function that takes any non-negative integer as an argument and returns it with its digits in descending order.
 
-Ваша задача – написать функцию, которая принимает любое неотрицательное целое число и возвращает его цифры, расположенные в порядке убывания.
-Проще говоря, нужно переставить цифры так, чтобы получилось наибольшее возможное число.
+Examples:
+Input: 42145
+Output: 54421
+
+Input: 145263
+Output: 654321
+
+Вам нужно написать функцию, которая принимает любое неотрицательное целое число и возвращает его цифры, расположенные в порядке убывания.
+
+Примеры:
+Ввод: 42145
+Вывод: 54421
+
+Ввод: 145263
+Вывод: 654321
+
+https://www.codewars.com/kata/5467e4d82edf8bbf40000155
 */
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-int compare(const void *number_1, const void *number_2) {
-    return (*(char *)number_2 - *(char *)number_1);
+int compare_desc(const void *a, const void *b) {
+    return (*(int *)b - *(int *)a);
 }
 
-long long descending_order(long long given_number) {
-    char string[20];
-    sprintf(string, "%lld", given_number);
-    
-    size_t length = strlen(string);
-    qsort(string, length, sizeof(char), compare);
-    
-    return atoll(string);
+int sort_digits_descending(int number) {
+    int digit_array[10];
+    int digit_count = 0;
+
+    while (number > 0) {
+        digit_array[digit_count++] = number % 10;
+        number /= 10;
+    }
+
+    // Sort digits in descending order
+    qsort(digit_array, digit_count, sizeof(int), compare_desc);
+
+    int result = 0;
+    for (int i = 0; i < digit_count; ++i) {
+        result = result * 10 + digit_array[i];
+    }
+
+    return result;
 }
 
 int main() {
-    printf("%lld\n", descending_order(123456789));
+    int input;
+    scanf("%d", &input);
+
+    int sorted = sort_digits_descending(input);
+    printf("%d\n", sorted);
+
     return 0;
 }
