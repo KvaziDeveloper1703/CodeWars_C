3c271c4ea220a83927ac54cfa93ba00e6f7784e5
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-08 03:15:10 +0300

    Modified CW_7_2

--- level_7/CW_7_2.c
+++ level_7/CW_7_2.c
@@ -1,30 +1,32 @@
 /*
-Your task is to write a function that takes a string and returns a new string with all vowels removed.
+You need to write a function that takes a string and returns a new string with all vowels removed.
 
 Example:
 "This website is for losers LOL!" -> "Ths wbst s fr lsrs LL!"
 
-Ваша задача – написать функцию, которая принимает строку и возвращает новую строку, из которой удалены все гласные.
+Ваша нужно написать функцию, которая принимает строку и возвращает новую строку, из которой удалены все гласные.
 
 Пример:
 "This website is for losers LOL!" -> "Ths wbst s fr lsrs LL!"
+
+https://www.codewars.com/kata/52fba66badcd10859f00097e
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-char *disemvowel(const char *given_string)
+char *remove_vowels(const char *given_string)
 {
 	int length = strlen(given_string);
-    char *result = (char *)malloc(length + 1);
+    char *result = malloc(length + 1);
     if (!result) return NULL;
     
     int j = 0;
     for (int i = 0; i < length; i++) {
         switch (given_string[i]) {
-            case 'a': case 'e': case 'i': case 'o': case 'u':
-            case 'A': case 'E': case 'I': case 'O': case 'U':
+            case 'a': case 'e': case 'i': case 'o': case 'u': case 'y':
+            case 'A': case 'E': case 'I': case 'O': case 'U': case 'Y':
                 break;
             default:
                 result[j++] = given_string[i];
