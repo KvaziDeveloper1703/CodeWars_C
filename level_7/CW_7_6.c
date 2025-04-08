8d32a89f119a2dfa437ac8f3003eac731ff1c72d
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-08 23:50:12 +0300

    Modified CW_7_6

--- level_7/CW_7_6.c
+++ level_7/CW_7_6.c
@@ -6,28 +6,36 @@ You are going to be given a non-empty string. Your job is to return the middle c
 Вам будет дана непустая строка. Ваша задача – вернуть средний символ(ы) этой строки.
 + Если длина строки нечетная, вернуть один средний символ.
 + Если длина строки четная, вернуть два средних символа.
+
+https://www.codewars.com/kata/56747fd5cb988479af000028
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-void get_middle(char *given_string, char *result) {
-    size_t length = strlen(given_string);
-    if (length % 2 == 0) {
-        result[0] = given_string[length / 2 - 1];
-        result[1] = given_string[length / 2];
-        result[2] = '\0';
+char* get_middle(const char *input_string) {
+    size_t input_length = strlen(input_string);
+    char *middle_characters = malloc((input_length % 2 == 0 ? 3 : 2));
+    if (!middle_characters) return NULL;
+
+    if (input_length % 2 == 0) {
+        middle_characters[0] = input_string[input_length / 2 - 1];
+        middle_characters[1] = input_string[input_length / 2];
+        middle_characters[2] = '\0';
     } else {
-        result[0] = given_string[length / 2];
-        result[1] = '\0';
+        middle_characters[0] = input_string[input_length / 2];
+        middle_characters[1] = '\0';
     }
+
+    return middle_characters;
 }
 
 int main() {
-    char result[3];
-    
-    get_middle("test", result);
-    printf("%s\n", result);
-    
+    char *middle_result = get_middle("test");
+    if (middle_result) {
+        printf("%s\n", middle_result);
+        free(middle_result);
+    }
     return 0;
 }
